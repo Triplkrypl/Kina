c118df88cd4af0b0c5aefa3ed83a192ee267fde5
Votech Brozek <triplkrypl@centrum.cz>
2016-11-21 21:34:01 +0100

    turn off log to console by config

--- bin/Util/Log.php
+++ bin/Util/Log.php
@@ -1,7 +1,9 @@
 <?php
 namespace Util;
 class Log{
+ private $config;
  public function __construct(\Server\Config $config){
+  $this->config = $config;
  }
  public function log($message,$log_type = "",$plugin = "Kina"){
   $format_message = "[".(new \DateTime())->format("d-m-Y H:i:s")."] [".$plugin."]";
@@ -9,7 +11,9 @@ class Log{
    $format_message .= " [".$log_type."]";
   }
   $format_message .= " ".$message."\n";
-  echo $format_message;
+  if($this->config->get("console")){
+   echo $format_message;
+  }
  }
  public function logException($add_message,\Exception $e,$log_type = "",$plugin = "Kina"){
   $message = $add_message." '".\get_class($e)."' message: ".$e->getMessage()." from file: ".$e->getFile()." line: ".$e->getLine();
